b598f9db92b17a5752536266dd8f8b0921bdc177
jbrammer@callibrity.com <jbrammer@callibrity.com>
2018-04-17 21:23:00 -0400

    imported ng-bootstrap and pulled a searching message out for reuse, doesn't look like I think it should so it'll need some work, but at a basic level the implementation itself works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoadingAlert } from './styles/loading-alert/loading-alert'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeroDetailComponent,
+    LoadingAlert
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    NgbModule.forRoot()
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
 import { HeroService } from '../../services/hero.service';
 import { Hero } from '../../models/hero';
 import 'rxjs/add/operator/catch';
 import { Observable } from 'rxjs/Rx';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
+})
+
+@NgModule({
+  imports: [NgbModule]
 })
 
 export class HomeComponent implements OnInit {
@@ -29,17 +33,19 @@ export class HomeComponent implements OnInit {
     this.searching = true;
     this.error = null;
     this.hero = null;
-    this.heroService.GetHero(this.model.search).catch(error => {
-      this.error = error;
-      this.searching = false;
-      return Observable.throw(error);
-    }).subscribe(hero => {      
-      if(!hero){
-        this.error = "What you searched for didn't work human. Try again!"
-      } else {
-        this.hero = hero;         
-      }
-      this.searching = false;
+    this.heroService.GetHero(this.model.search)
+      .catch(error => {
+        this.error = error;
+        this.searching = false;
+        return Observable.throw(error);
+      })
+      .subscribe(hero => {      
+        if(!hero){
+          this.error = "What you searched for didn't work human. Try again!"
+        } else {
+          this.hero = hero;         
+        }
+        this.searching = false;
     });    
   }
 }
